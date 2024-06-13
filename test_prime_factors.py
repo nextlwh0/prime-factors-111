810f49fea34ae0ed2e7726e06193b7a4dbabf745
whlee <nextlwh@daum.net>
2024-06-13 09:08:28 +0900

    first commit

---
+++ test_prime_factors.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from prime_factors import PrimeFactor
+
+
+class TestPrimeFactor(TestCase):
+    def test_prime_factor(self):
+        prime_factor = PrimeFactor()
+
