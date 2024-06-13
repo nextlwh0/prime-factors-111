0ff356394df8eda811594f3707e45f47b4989f16
whlee <nextlwh@daum.net>
2024-06-13 09:40:35 +0900

    [feature] 21 prime factor 1

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -4,6 +4,7 @@ from prime_factors import PrimeFactor
 
 
 class TestPrimeFactor(TestCase):
-    def test_prime_factor(self):
+    def test_prime_factor_of_1(self):
         prime_factor = PrimeFactor()
+        self.assertEqual([], prime_factor.of(1))
 
